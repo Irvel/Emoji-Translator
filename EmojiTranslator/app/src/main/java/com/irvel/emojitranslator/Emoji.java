1c852b39f8f86cea04312e8d1dfaee5ebeda5368
Irvel <irvelnduva@hotmail.com>
2015-09-30 05:23:40 -0500

    Tweak smiling treshold to display more the Expressionless emoji

--- EmojiTranslator/app/src/main/java/com/irvel/emojitranslator/Emoji.java
+++ EmojiTranslator/app/src/main/java/com/irvel/emojitranslator/Emoji.java
@@ -29,10 +29,62 @@ public class Emoji {
         calculateEmoji();
     }
 
+    //==============================================================================================
+    // Calculate Emoji
+    //==============================================================================================
+
+    /**
+     * Calculates the corresponding emoji based on the probabilities of the open left eye,
+     * open right eye and smile. The emoji values are currently hard-coded because linking
+     * them a string resource triggers a force-close.
+     * TODO: Implement a more robust emoji-face model
+     */
+
     private void calculateEmoji() {
         //If there is no smile, display a sad emoji
         if(mSmiling == Face.UNCOMPUTED_PROBABILITY){
-            mEmojiChar = "\uD83D\uDE1F";
+            if(mLeftEyeOpen > .8 && mRightEyeOpen > .8) {
+                //Worried Face
+                mEmojiChar = "\uD83D\uDE1F";
+            }
+            else if((mLeftEyeOpen > .3 && mLeftEyeOpen < .8) && (mRightEyeOpen > .3 && mRightEyeOpen < .8) ) {
+                //Astonished Face
+                mEmojiChar = "\uD83D\uDE32";
+            }
+            else{
+                //Disappointed Face
+                mEmojiChar = "\uD83D\uDE1E";
+            }
+        }
+        else{
+            if(mLeftEyeOpen > .8 && mRightEyeOpen > .8) {
+                if(mSmiling > .7) {
+                    //Smiling Face With Open Mouth and Smiling Eyes
+                    mEmojiChar = "\uD83D\uDE04";
+                }
+                else{
+                    //Grinning Face With Smiling Eyes
+                    mEmojiChar = "\uD83D\uDE01";
+                }
+            }
+            else if((mLeftEyeOpen > .3 && mLeftEyeOpen < .8) && (mRightEyeOpen > .3 && mRightEyeOpen < .8) ) {
+                //Grinning Face
+                mEmojiChar = "\uD83D\uDE00";
+            }
+            else if(mLeftEyeOpen < .5 && mRightEyeOpen > .5 || mLeftEyeOpen > .5 && mRightEyeOpen < .5 ) {
+                //Winking Face
+                mEmojiChar = "\uD83D\uDE09";
+            }
+            else{
+                if(mSmiling > .45){
+                    //Smiling Face With Open Mouth and Tightly-Closed Eyes
+                    mEmojiChar = "\uD83D\uDE06";
+                }
+                else{
+                    //Expressionless Face
+                    mEmojiChar = "\uD83D\uDE11";
+                }
+            }
         }
     }
 }
